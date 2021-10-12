8969d84b982eb5f776d015f7abea370fcda7fd58
Meatware Monster <meatwaremonster@gmail.com>
2021-10-12 18:04:58 -0400

    Add ballista model and start projectile setup.

---
+++ MoreDefenses/Scripts/ProjectileParticle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileParticle : MonoBehaviour
+{
+    private ParticleSystem m_particleSystem;
+    private List<ParticleCollisionEvent> m_particleCollisionEvents = new List<ParticleCollisionEvent>();
+
+    private void Start()
+    {
+        m_particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        var events = m_particleSystem.GetCollisionEvents(other, m_particleCollisionEvents);
+
+        for (var i = 0; i < events; i++)
+        {
+
+        }
+    }
+}
